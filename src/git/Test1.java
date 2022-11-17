b2f66dcac2057e43d5dfdcf656c62c68c4dc273f
moham <moham@192.168.2.62>
2022-11-17 03:00:49 -0500

    First commit

---
+++ src/git/Test1.java
@@ -0,0 +1,15 @@
+package git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int age=18;
+				if (age==18)
+					System.out.println("you can vote");
+					else {
+						System.out.println("you are minor");
+					}
+
+	}
+
+}
